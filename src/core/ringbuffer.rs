bb38dda7b60eda3b09ba461f67a4291d5cb3c3ac
Michael Nye <thenyeguy@gmail.com>
2014-11-09 23:20:31 -0500

    Update function and documentation for ringbuffer

--- src/core/ringbuffer.rs
+++ src/core/ringbuffer.rs
@@ -1,6 +1,10 @@
-//! A generic ringbuffer
+//! A generic ring buffer
+//!
+//! A ring buffer can continue adding data on to the end of itself indefinitely.
+//! However, it has a limited capacity; when that capacity is reached, it will
+//! remove the oldest data to make space.
 
-#![experimental]
+#![unstable]
 
 use std::clone::Clone;
 use std::vec::Vec;
@@ -8,6 +12,7 @@ use std::vec::Vec;
 use core::Time;
 
 
+/// A generic ring buffer
 pub struct RingBuffer<T: Clone> {
     buf: Vec<T>,
     capacity: uint,
@@ -17,6 +22,7 @@ pub struct RingBuffer<T: Clone> {
 }
 
 impl<T: Clone> RingBuffer<T> {
+    /// Returns an empty ring buffer that can hold at most `capacity` elements.
     pub fn new(capacity: uint) -> RingBuffer<T> {
         RingBuffer { 
             buf: Vec::with_capacity(capacity), 
@@ -27,6 +33,8 @@ impl<T: Clone> RingBuffer<T> {
         }
     }
 
+    /// Attempts to return the data stored at time `t`. If the requested time is
+    /// not in the buffer, instead returns `None`.
     pub fn get(&self, t: Time) -> Option<T> {
         if self.start_t <= t && t < self.end_t {
             Some(self.buf[(t % self.capacity as Time) as uint].clone())
@@ -35,6 +43,8 @@ impl<T: Clone> RingBuffer<T> {
         }
     }
 
+    /// Pushes the supplied data onto the end of the buffer. If the buffer is
+    /// full, this will overwrite the oldest data.
     pub fn push(&mut self, data: T) {
         if self.size < self.capacity {
             self.buf.push(data);
@@ -44,6 +54,17 @@ impl<T: Clone> RingBuffer<T> {
             self.buf[(self.end_t % self.capacity as Time) as uint] = data;
             self.start_t += 1;
             self.end_t += 1;
+        }
+    }
+
+    /// Attempts to change the value at time `t` to `data`. If the requested
+    /// time is not in the buffer, instead returns `Err`.
+    pub fn update(&mut self, t: Time, data: T) -> Result<(),()> {
+        if self.start_t <= t && t < self.end_t {
+            self.buf[(t % self.capacity as Time) as uint] = data;
+            Ok(())
+        } else {
+            Err(())
         }
     }
 }
@@ -100,5 +121,26 @@ mod tests {
         assert_eq!(rb.get(6), Some(7));
         assert_eq!(rb.get(7), Some(13));
         assert_eq!(rb.get(8), None);
+    }
+
+    #[test]
+    fn test_update() {
+        let mut rb: RingBuffer<int> = RingBuffer { 
+            buf: vec![7,13], 
+            capacity: 2, 
+            size: 2,
+            start_t: 7,
+            end_t: 9
+        };
+
+        // Test out of range
+        assert_eq!(rb.update(6, 22), Err(()));
+        assert_eq!(rb.update(9, 22), Err(()));
+
+        // Test in range
+        assert_eq!(rb.update(7, 22), Ok(()));
+        assert_eq!(rb.buf[1], 22);
+        assert_eq!(rb.update(8, 23), Ok(()));
+        assert_eq!(rb.buf[0], 23);
     }
 }
