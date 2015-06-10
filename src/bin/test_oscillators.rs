439f4d38ae6108687789fafee5e4264bcff8d339
Michael Nye <thenyeguy@gmail.com>
2015-06-09 21:33:30 -0400

    Upgrade test oscillators to use chains

--- src/bin/test_oscillators.rs
+++ src/bin/test_oscillators.rs
@@ -4,47 +4,46 @@ extern crate oxcable;
 
 #[cfg(not(test))]
 fn main() {
-    use std::vec::Vec;
-    use oxcable::types::Device;
+    use oxcable::chain::DeviceChain;
     use oxcable::io::wav::WavWriter;
-    use oxcable::oscillator;
-    use oxcable::oscillator::Oscillator;
+    use oxcable::oscillator::{self, Oscillator};
 
     // Initialize objects
     println!("Initializing signal chain...");
     let freq = 8000.0;
-    let mut oscs = Vec::<oscillator::Oscillator>::new();
-    let mut wavs = Vec::<WavWriter>::new();
-    oscs.push(Oscillator::new(oscillator::Sine, freq));
-    wavs.push(WavWriter::new("wav/test_sine.wav", 1));
-    oscs.push(Oscillator::new(oscillator::Saw(oscillator::Aliased), freq));
-    wavs.push(WavWriter::new("wav/test_saw_naive.wav", 1));
-    oscs.push(Oscillator::new(oscillator::Saw(oscillator::PolyBlep), freq));
-    wavs.push(WavWriter::new("wav/test_saw_blep.wav", 1));
-    oscs.push(Oscillator::new(oscillator::Square(oscillator::Aliased), freq));
-    wavs.push(WavWriter::new("wav/test_square_naive.wav", 1));
-    oscs.push(Oscillator::new(oscillator::Square(oscillator::PolyBlep), freq));
-    wavs.push(WavWriter::new("wav/test_square_blep.wav", 1));
-    oscs.push(Oscillator::new(oscillator::Tri(oscillator::Aliased), freq));
-    wavs.push(WavWriter::new("wav/test_tri_naive.wav", 1));
-    oscs.push(Oscillator::new(oscillator::Tri(oscillator::PolyBlep), freq));
-    wavs.push(WavWriter::new("wav/test_tri_blep.wav", 1));
-    oscs.push(Oscillator::new(oscillator::WhiteNoise, freq));
-    wavs.push(WavWriter::new("wav/test_white_noise.wav", 1));
-    oscs.push(Oscillator::new(oscillator::PulseTrain, freq));
-    wavs.push(WavWriter::new("wav/test_pulse_train.wav", 1));
-
-    // Link oscillators to wav outs
-    println!("Writing oscillators to wav files...");
-    for i in (0 .. oscs.len()) {
-        wavs[i].inputs.set_channel(0, oscs[i].output.get_channel());
-    }
+    let mut chains: Vec<DeviceChain> = Vec::new();
+    chains.push(DeviceChain::from(Oscillator::new(oscillator::Sine, freq))
+        .into(WavWriter::new("wav/test_sine.wav", 1)));
+    chains.push(DeviceChain::from(
+            Oscillator::new(oscillator::Saw(oscillator::Aliased), freq))
+        .into(WavWriter::new("wav/test_saw_naive.wav", 1)));
+    chains.push(DeviceChain::from(
+            Oscillator::new(oscillator::Saw(oscillator::PolyBlep), freq))
+        .into(WavWriter::new("wav/test_saw_blep.wav", 1)));
+    chains.push(DeviceChain::from(
+            Oscillator::new(oscillator::Square(oscillator::Aliased), freq))
+        .into(WavWriter::new("wav/test_square_naive.wav", 1)));
+    chains.push(DeviceChain::from(
+            Oscillator::new(oscillator::Square(oscillator::PolyBlep), freq))
+        .into(WavWriter::new("wav/test_square_blep.wav", 1)));
+    chains.push(DeviceChain::from(
+            Oscillator::new(oscillator::Tri(oscillator::Aliased), freq))
+        .into(WavWriter::new("wav/test_tri_naive.wav", 1)));
+    chains.push(DeviceChain::from(
+            Oscillator::new(oscillator::Tri(oscillator::PolyBlep), freq))
+        .into(WavWriter::new("wav/test_tri_blep.wav", 1)));
+    chains.push(DeviceChain::from(
+            Oscillator::new(oscillator::WhiteNoise, freq))
+        .into(WavWriter::new("wav/test_white_noise.wav", 1)));
+    chains.push(DeviceChain::from(
+            Oscillator::new(oscillator::PulseTrain, freq))
+        .into(WavWriter::new("wav/test_pulse_train.wav", 1)));
 
     // Write files
-    for t in (0u64 .. 44100) {
-        for i in (0 .. oscs.len()) {
-            oscs[i].tick(t);
-            wavs[i].tick(t);
+    println!("Writing oscillators to wav files...");
+    for _ in (0u64 .. 44100) {
+        for chain in chains.iter_mut() {
+            chain.tick();
         }
     }
     println!("Done");
