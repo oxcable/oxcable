41ca1b165b87916dfce3c780563476f8ca10c22e
Michael Nye <thenyeguy@gmail.com>
2015-06-14 20:17:38 -0400

    Add cycle detection to DeviceGraph
    This not only prevents creating cyclic graphs in the first place, but it also
    ensures that devices are ALWAYS ticked in the correct order, meaning they don't
    have to be added in the right order by the user.

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -16,17 +16,16 @@ fn main() {
     println!("Setting up signal chain...");
     let engine = AudioEngine::open().unwrap();
     let mut graph = DeviceGraph::new();
+    let spk = graph.add_node(engine.new_output(2));
 
     let mic = graph.add_node(engine.new_input(1));
     let del = graph.add_node(Delay::new(0.5, 0.5, 0.5, 1));
     graph.add_edge(mic, 0, del, 0).unwrap();
+    graph.add_edge(del, 0, spk, 0).unwrap();
 
     let osc = graph.add_node(Oscillator::new(oscillator::Sine, 440.0));
     let gain = graph.add_node(Gain::new(-12.0, 1));
     graph.add_edge(osc, 0, gain, 0).unwrap();
-
-    let spk = graph.add_node(engine.new_output(2));
-    graph.add_edge(del, 0, spk, 0).unwrap();
     graph.add_edge(gain, 0, spk, 1).unwrap();
 
     println!("Playing. Press Enter to quit...");
--- src/graph.rs
+++ src/graph.rs
@@ -1,18 +1,22 @@
 //! Provides a way to link `AudioDevice`s into an acyclic graph.
 
+use std::collections::VecDeque;
+
 use types::{AudioDevice, DeviceIOType, Sample, Time};
 use utils::tick::Tick;
 
 pub struct DeviceGraph {
-    devices: Vec<AudioNode>,
-    bus: Vec<Sample>,
-    time: Time
+    nodes: Vec<AudioNode>, // the actual nodes
+    topology: Vec<usize>, // the order to tick the nodes
+    bus: Vec<Sample>, // the audio bus to write samples to
+    time: Time // the next timestep
 }
 
 impl DeviceGraph {
     pub fn new() -> DeviceGraph {
         DeviceGraph {
-            devices: Vec::new(),
+            nodes: Vec::new(),
+            topology: Vec::new(),
             bus: Vec::new(),
             time: 0
         }
@@ -21,8 +25,10 @@ impl DeviceGraph {
     pub fn add_node<D>(&mut self, device: D) -> AudioNodeIdx
             where D: 'static+AudioDevice {
         let node = AudioNode::new(device, &mut self.bus);
-        self.devices.push(node);
-        AudioNodeIdx(self.devices.len()-1)
+        let idx = self.nodes.len();
+        self.nodes.push(node);
+        self.topology.push(idx);
+        AudioNodeIdx(idx)
     }
 
     pub fn add_edge(&mut self, from: AudioNodeIdx, from_ch: usize,
@@ -30,41 +36,94 @@ impl DeviceGraph {
         // Check device indices
         let AudioNodeIdx(from_i) = from;
         let AudioNodeIdx(to_i) = to;
-        if from_i >= self.devices.len() {
+        if from_i >= self.nodes.len() {
             return Err(GraphError::FromOutOfRange);
-        } else if to_i >= self.devices.len() {
+        } else if to_i >= self.nodes.len() {
             return Err(GraphError::ToOutOfRange);
         }
 
         // Check channels
-        match self.devices[from_i].device.num_outputs() {
+        match self.nodes[from_i].device.num_outputs() {
             DeviceIOType::Exactly(i) if from_ch >= i =>
                 return Err(GraphError::FromChOutOfRange),
             _ => ()
         }
-        match self.devices[to_i].device.num_inputs() {
+        match self.nodes[to_i].device.num_inputs() {
             DeviceIOType::Exactly(i) if to_ch >= i =>
                 return Err(GraphError::ToChOutOfRange),
             _ => ()
         }
-        while self.devices[to_i].inputs.len() < to_ch {
-            self.devices[to_i].inputs.push(None);
+        while self.nodes[to_i].inputs.len() < to_ch {
+            self.nodes[to_i].inputs.push(None);
         }
 
         // Set input
-        let (start,_) = self.devices[from_i].outputs;
-        self.devices[to_i].inputs[to_ch] = Some(start+from_ch);
-        Ok(())
+        let (start,_) = self.nodes[from_i].outputs;
+        self.nodes[to_i].inputs[to_ch] = Some(start+from_ch);
+        self.topological_sort(to_i, to_ch)
+    }
+
+    /// Determines the topology of our device graph. If the graph has a cycle,
+    /// then we remove the last edge. Otherwise, we set self.topology to
+    /// a topologically sorted order.
+    fn topological_sort(&mut self, to_i: usize, to_ch: usize) ->
+            Result<(), GraphError> {
+        // Intialize our set of input edges, and our set of edgeless nodes
+        let mut topology = Vec::new();
+        let mut inputs: Vec<Vec<_>> = self.nodes.iter().map(
+            |node| node.inputs.iter().filter_map(|&o| o).collect()
+        ).collect();
+        let mut no_inputs: VecDeque<_> = inputs.iter().enumerate().filter_map(
+            |(i, ins)| if ins.len() == 0 { Some(i) } else { None }
+        ).collect();
+
+        // While there are nodes with no input, we choose one, add it as the
+        // next node in our topology, and remove all edges from that node. Any
+        // nodes that lose their final edge are added to the edgeless set.
+        loop {
+            match no_inputs.pop_front() {
+                Some(i) => {
+                    topology.push(i);
+                    for (j, ins) in inputs.iter_mut().enumerate() {
+                        let mut idx = None;
+                        for k in 0..ins.len() {
+                            if ins[k] == i {
+                                idx = Some(k);
+                                break;
+                            }
+                        }
+                        match idx {
+                            Some(k) => {
+                                ins.swap_remove(k);
+                                if ins.len() == 0 {
+                                    no_inputs.push_back(j);
+                                }
+                            },
+                            None => ()
+                        }
+                    }
+                },
+                None => break
+            }
+        }
+
+        if topology.len() == self.nodes.len() {
+            self.topology = topology;
+            Ok(())
+        } else {
+            self.nodes[to_i].inputs[to_ch] = None;
+            Err(GraphError::CreatesCycle)
+        }
     }
 }
 
 impl Tick for DeviceGraph {
     fn tick(&mut self) {
-        for i in 0..self.devices.len() {
-            let inputs: Vec<Sample> = self.devices[i].inputs.iter().map(|input|
+        for &i in self.topology.iter() {
+            let inputs: Vec<Sample> = self.nodes[i].inputs.iter().map(|input|
                 input.map_or(0.0, |ch| self.bus[ch])).collect();
-            let (start, end) = self.devices[i].outputs;
-            self.devices[i].device.tick(self.time, &inputs,
+            let (start, end) = self.nodes[i].outputs;
+            self.nodes[i].device.tick(self.time, &inputs,
                                         &mut self.bus[start..end]);
         }
         self.time += 1;
@@ -73,8 +132,7 @@ impl Tick for DeviceGraph {
 
 #[derive(Copy, Clone, Debug)]
 pub enum GraphError {
-    FromOutOfRange, FromChOutOfRange,
-    ToOutOfRange, ToChOutOfRange
+    FromOutOfRange, FromChOutOfRange, ToOutOfRange, ToChOutOfRange, CreatesCycle
 }
 
 #[derive(Copy, Clone, Debug)]
