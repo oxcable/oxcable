4f87347257d30cc2c5fa0b77bb34cbc316af7b56
Michael Nye <thenyeguy@gmail.com>
2015-06-17 20:23:41 -0400

    Bug fixes and tests for voice selection

--- src/voice_array.rs
+++ src/voice_array.rs
@@ -11,26 +11,27 @@ pub struct VoiceArray<T> {
     voices: Vec<T>,
     /// Maps MIDI note numbers to currently triggered voice indices
     note_to_voice: HashMap<u8, usize>,
-    /// Maps voice indices to MIDI note numbers
-    voice_to_note: HashMap<usize, u8>,
-    /// Places the most recently mapped voices at the end
-    voice_queue: VecDeque<usize>
+    /// Places the most recently mapped voices at the end, and track the note
+    /// they are currently playing
+    held_voices: VecDeque<(usize, u8)>,
+    /// Tracks free voices
+    free_voices: VecDeque<usize>,
 }
 
 impl<T> VoiceArray<T> {
     /// Creates a new VoiceArray from the provifed vector of voices.
     pub fn new(voices: Vec<T>) -> VoiceArray<T> {
         let num_voices = voices.len();
-        let mut voice_queue = VecDeque::new();
+        let mut free_voices = VecDeque::new();
         for i in (0 .. num_voices) {
-            voice_queue.push_back(i);
+            free_voices.push_back(i);
         }
 
         VoiceArray {
             voices: voices,
             note_to_voice: HashMap::new(),
-            voice_to_note: HashMap::new(),
-            voice_queue: voice_queue
+            held_voices: VecDeque::new(),
+            free_voices: free_voices,
         }
     }
 
@@ -46,31 +47,30 @@ impl<T> VoiceArray<T> {
 
     /// Selects a new voice, marks it as used, and loans it out
     pub fn note_on(&mut self, note: u8) -> &mut T {
-        let i = match self.note_to_voice.remove(&note) {
-            Some(i) => {
+        let i = match self.note_to_voice.get(&note) {
+            Some(&i) => {
                 // This note is already being played, so retrigger it and move
                 // it to the back of the queue
                 self.remove_from_queue(i);
                 i
             },
             None => {
-                // This note is not being played, so get the next voice
-                let i = self.voice_queue.pop_front().unwrap();
-
-                // Remove the mapping between the voice and its old note
-                match self.voice_to_note.remove(&i) {
-                    Some(n) => { self.note_to_voice.remove(&n); },
-                    None => ()
-                }
-
-                // Map this voice to its new note
+                let i = match self.free_voices.pop_front() {
+                    // If there is a free voice, use the oldest one
+                    Some(i) => i,
+                    // Otherwise, use the oldest playing voice
+                    None => {
+                        let (i, n) = self.held_voices.pop_front().unwrap();
+                        self.note_to_voice.remove(&n);
+                        i
+                    }
+                };
                 self.note_to_voice.insert(note, i);
-                self.voice_to_note.insert(i, note);
                 i
             }
         };
         // Finally, push the voice to the back of the queue and handle the event
-        self.voice_queue.push_back(i);
+        self.held_voices.push_back((i,note));
         &mut self.voices[i]
     }
 
@@ -79,8 +79,7 @@ impl<T> VoiceArray<T> {
         match self.note_to_voice.remove(&note) {
             Some(i) => {
                 self.remove_from_queue(i);
-                self.voice_to_note.remove(&i);
-                self.voice_queue.push_front(i);
+                self.free_voices.push_back(i);
                 Some(&mut self.voices[i])
             },
             None => None
@@ -89,11 +88,86 @@ impl<T> VoiceArray<T> {
 
     // Finds a voice in the queue and removes it
     fn remove_from_queue(&mut self, voice: usize) {
-        for i in (0 .. self.voice_queue.len()) {
-            if *self.voice_queue.get(i).unwrap() == voice {
-                self.voice_queue.remove(i);
+        for i in (0 .. self.held_voices.len()) {
+            let (j, _) = self.held_voices[i];
+            if j == voice {
+                self.held_voices.remove(i);
                 break;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    /// Verify that asking for a new note selects free voices first
+    #[test]
+    fn test_free_voice() {
+        use super::VoiceArray;
+        let mut voices1 = VoiceArray::new(vec![1,2]);
+        let v1 = voices1.note_on(1).clone();
+        let _ = voices1.note_on(2).clone();
+        voices1.note_off(1);
+        let v3 = voices1.note_on(3).clone();
+        assert_eq!(v1, v3);
+
+        let mut voices2 = VoiceArray::new(vec![1,2]);
+        let _ = voices2.note_on(1).clone();
+        let v2 = voices2.note_on(2).clone();
+        voices2.note_off(2);
+        let v3 = voices2.note_on(3).clone();
+        assert_eq!(v2, v3);
+    }
+
+    /// Verify that asking for a held note always returns that held note
+    #[test]
+    fn test_key_repeat() {
+        use super::VoiceArray;
+        let mut voices = VoiceArray::new(vec![1,2]);
+        let v1 = voices.note_on(1).clone();
+        let v2 = voices.note_on(2).clone();
+        let v3 = voices.note_on(1).clone();
+        let v4 = voices.note_on(1).clone();
+        let v5 = voices.note_on(2).clone();
+        let v6 = voices.note_on(2).clone();
+        assert!(v1 != v2);
+        assert_eq!(v1, v3);
+        assert_eq!(v1, v4);
+        assert_eq!(v2, v5);
+        assert_eq!(v2, v6);
+    }
+
+    /// Verify that the oldest free voice is always selected
+    #[test]
+    fn test_oldest_free() {
+        use super::VoiceArray;
+        let mut voices = VoiceArray::new(vec![1,2]);
+        let v1 = voices.note_on(1).clone();
+        let v2 = voices.note_on(2).clone();
+
+        voices.note_off(1);
+        voices.note_off(2);
+        let v3 = voices.note_on(3).clone();
+        assert_eq!(v3, v1);
+
+        voices.note_off(3);
+        let v4 = voices.note_on(4).clone();
+        assert_eq!(v4, v2);
+    }
+
+    /// Verify that note pruning always selects the oldest held voice
+    #[test]
+    fn test_oldest_held() {
+        use super::VoiceArray;
+        let mut voices = VoiceArray::new(vec![1,2,3]);
+        let v1 = voices.note_on(1).clone();
+        let v2 = voices.note_on(2).clone();
+        let v3 = voices.note_on(3).clone();
+        let v4 = voices.note_on(4).clone();
+        let v5 = voices.note_on(5).clone();
+        let v6 = voices.note_on(6).clone();
+        assert_eq!(v1, v4);
+        assert_eq!(v2, v5);
+        assert_eq!(v3, v6);
     }
 }
