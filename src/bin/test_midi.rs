00336fe0aa62a2fad4c9715329c4c3d1077d8530
Michael Nye <michael.nye@vocitec.com>
2015-07-20 09:44:56 -0400

    Remove unused warnings when running tests

--- src/bin/test_midi.rs
+++ src/bin/test_midi.rs
@@ -2,45 +2,51 @@
 
 extern crate oxcable;
 
-use oxcable::adsr;
-use oxcable::chain::{DeviceChain, Tick};
-use oxcable::types::{AudioDevice, MidiDevice, MidiMessage, Time, Sample};
-use oxcable::io::audio::AudioEngine;
-use oxcable::io::midi::{MidiEngine, MidiIn};
-use oxcable::oscillator::{self, Oscillator};
 
+#[cfg(not(test))]
+mod wrapper {
+    use oxcable::adsr;
+    use oxcable::io::midi::MidiIn;
+    use oxcable::types::{AudioDevice, MidiDevice, MidiMessage, Time, Sample};
 
-struct WrappedAdsr {
-    midi: MidiIn,
-    adsr: adsr::Adsr
-}
-impl AudioDevice for WrappedAdsr {
-    fn num_inputs(&self) -> usize {
-        self.adsr.num_inputs()
+    pub struct WrappedAdsr {
+        pub midi: MidiIn,
+        pub adsr: adsr::Adsr
     }
-
-    fn num_outputs(&self) -> usize {
-        self.adsr.num_outputs()
-    }
-
-    fn tick(&mut self, t: Time, inputs: &[Sample], outputs: &mut[Sample]) {
-        for event in self.midi.get_events(t) {
-            println!("{:?}", event);
-            match event.payload {
-                MidiMessage::NoteOn(_,_) =>
-                    self.adsr.handle_message(adsr::NoteDown),
-                MidiMessage::NoteOff(_,_) =>
-                    self.adsr.handle_message(adsr::NoteUp),
-                _ => ()
-            }
+    impl AudioDevice for WrappedAdsr {
+        fn num_inputs(&self) -> usize {
+            self.adsr.num_inputs()
         }
-        self.adsr.tick(t, inputs, outputs);
+
+        fn num_outputs(&self) -> usize {
+            self.adsr.num_outputs()
+        }
+
+        fn tick(&mut self, t: Time, inputs: &[Sample], outputs: &mut[Sample]) {
+            for event in self.midi.get_events(t) {
+                println!("{:?}", event);
+                match event.payload {
+                    MidiMessage::NoteOn(_,_) =>
+                        self.adsr.handle_message(adsr::NoteDown),
+                    MidiMessage::NoteOff(_,_) =>
+                        self.adsr.handle_message(adsr::NoteUp),
+                    _ => ()
+                }
+            }
+            self.adsr.tick(t, inputs, outputs);
+        }
     }
 }
 
 
 #[cfg(not(test))]
 fn main() {
+    use oxcable::adsr::Adsr;
+    use oxcable::chain::{DeviceChain, Tick};
+    use oxcable::io::audio::AudioEngine;
+    use oxcable::io::midi::MidiEngine;
+    use oxcable::oscillator::{self, Oscillator};
+
     println!("Initializing signal chain...");
     let audio_engine = AudioEngine::with_buffer_size(256).unwrap();
     let midi_engine = MidiEngine::open().unwrap();
@@ -48,9 +54,9 @@ fn main() {
     let mut chain = DeviceChain::from(
         Oscillator::new(oscillator::Saw(oscillator::PolyBlep)).freq(220.0)
     ).into(
-        WrappedAdsr {
+        wrapper::WrappedAdsr {
             midi: midi_engine.choose_input().unwrap(),
-            adsr: adsr::Adsr::default(1)
+            adsr: Adsr::default(1)
         }
     ).into(
         audio_engine.default_output(1).unwrap()
