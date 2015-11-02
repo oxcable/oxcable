e2a184583ef672fef2b3a533d45fa1a77257c8e0
Michael Nye <thenyeguy@gmail.com>
2015-11-01 20:29:06 -0800

    Implement Display for Error

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,6 @@
 //! Provides a standardized error type for oxcable.
 
+use std::fmt;
 use std::io;
 use std::result;
 
@@ -7,6 +8,10 @@ use byteorder;
 use portaudio::pa;
 use portmidi;
 
+/// A global error type for all oxcable operations.
+///
+/// Many of the errors simply wrap the errors provided by our supporting
+/// libraries.
 #[derive(Debug)]
 pub enum Error {
     /// No audio resources can be opened.
@@ -21,17 +26,18 @@ pub enum Error {
     OutOfRange(&'static str),
     /// The requested operation would create a graph cycle.
     CreatesCycle,
+    /// A feature isn't supported.
+    ///
+    /// The string argument describes what feature.
+    Unsupported(&'static str),
     /// A std::io operation failed.
     Io(io::Error),
     /// A portaudio operation failed.
     PortAudio(pa::Error),
     /// A portmidi operation failed.
     PortMidi(portmidi::PortMidiError),
-    /// A feature isn't supported.
-    ///
-    /// The string argument describes what feature.
-    Unsupported(&'static str),
 }
+use self::Error::*;
 
 impl From<byteorder::Error> for Error {
     fn from(e: byteorder::Error) -> Self {
@@ -59,5 +65,24 @@ impl From<portmidi::PortMidiError> for Error {
         Error::PortMidi(e)
     }
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &NoAudioDevices => write!(f, "No audio devices found."),
+            &NoMidiDevices => write!(f, "No midi devices found."),
+            &InvalidFile => write!(f, "The provided was not valid."),
+            &OutOfRange(ref idx) => write!(f, "Index `{}` was out of range.", idx),
+            &CreatesCycle =>
+                write!(f, "The requested action creates a graph cycle."),
+            &Unsupported(ref feat) =>
+                write!(f, "Unsupported feature: {}", feat),
+            &Io(ref e) => write!(f, "IO error: {}", e),
+            &PortAudio(ref e) => write!(f, "PortAudio error: {}", e),
+            &PortMidi(ref e) => write!(f, "PortMidi error: {:?}", e),
+        }
+    }
+}
+
 
 pub type Result<T> = result::Result<T, Error>;
