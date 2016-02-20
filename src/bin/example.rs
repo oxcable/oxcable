3a9f5cf45694b2c18e177a8574e860dfe3c71a77
Michael Nye <thenyeguy@gmail.com>
2016-02-19 21:45:54 -0800

    Update example

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -8,7 +8,7 @@ extern crate oxcable;
 #[cfg(not(test))]
 fn main() {
     use oxcable::oscillator;
-    use oxcable::delay::Delay;
+    use oxcable::filters::first_order::{Filter, LowPass};
     use oxcable::graph::{DeviceGraph, Tick};
     use oxcable::io::audio::AudioEngine;
     use oxcable::mixers::Gain;
@@ -17,21 +17,21 @@ fn main() {
     println!("Setting up signal chain...");
     let engine = AudioEngine::with_buffer_size(256).unwrap();
     let mut graph = DeviceGraph::new();
-    let spk = graph.add_node(engine.default_output(2).unwrap());
+    let speaker = graph.add_node(engine.default_output(2).unwrap());
 
-    let mic = graph.add_node(engine.default_input(1).unwrap());
-    let del = graph.add_node(Delay::new(0.5, 0.5, 0.5, 1));
-    graph.add_edge(mic, 0, del, 0).unwrap();
-    graph.add_edge(del, 0, spk, 0).unwrap();
+    let microphone = graph.add_node(engine.default_input(1).unwrap());
+    let filter = graph.add_node(Filter::new(LowPass(8000f32), 1));
+    graph.add_edge(microphone, 0, filter, 0).unwrap();
+    graph.add_edge(filter, 0, speaker, 0).unwrap();
 
     let lfo = graph.add_node(Oscillator::new(oscillator::Sine).freq(10.0));
-    let osc = graph.add_node(
+    let oscillator = graph.add_node(
         Oscillator::new(Tri(PolyBlep)).freq(440.0).lfo_intensity(0.1)
     );
     let gain = graph.add_node(Gain::new(-6.0, 1));
-    graph.add_edge(lfo, 0, osc, 0).unwrap();
-    graph.add_edge(osc, 0, gain, 0).unwrap();
-    graph.add_edge(gain, 0, spk, 1).unwrap();
+    graph.add_edge(lfo, 0, oscillator, 0).unwrap();
+    graph.add_edge(oscillator, 0, gain, 0).unwrap();
+    graph.add_edge(gain, 0, speaker, 1).unwrap();
 
     println!("Playing. Press Enter to quit...");
     graph.tick_until_enter();
